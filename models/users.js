126f3d4f8abe454e2413c1ed7c0341451d781001
MallamTY <sosanyatemitayonurudeen@gmail.com>
2022-09-27 12:52:24 -0700

    Implemented user registration Endpoint

--- models/users.js
+++ models/users.js
@@ -3,20 +3,40 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
  const userSchema = new schema({
+    firstname: {
+        type: String,
+         required: [true, `First name can't be empty`],
+         trim: true
+    },
+
+    lastname: {
+        type: String,
+        required: [true, `Last name filed can't be empty`],
+        trim: true
+    },
+
+    middlename: {                           
+        type: String,
+        min: 1,
+        trim: true
+    },
+
      username:{
          type: String,
          required: true,
          min: 5,
          max: 50,
-         unique: true
+         unique: true,
+         trim: true
      },
 
      email: {
          type: String,
          required: true,
          min: 5,
-         max: 50,
-         unique: true
+         max: [50, `Email address too long`],
+         unique: true,
+         trim: true
      },
 
      password:{
@@ -60,19 +80,29 @@ const schema = mongoose.Schema;
          max: 50
      },
 
-     from: {
+     country: {
         type: String,
-        max: 50
+        max: 50,
+        required: true,
+        trim: true
     },
 
     city: {
         type: String,
-        max: 50
+        max: 50,
+        required: true,
+        trim: true
+    },
+
+    gender: {                                                                   
+        type: String,
+        enum: [`Male`, `Female`, `Cross-dresser`],
+        required: true
     },
     
      relationship: {
          type: String,
-         enum: [1,2,3]
+         enum: [`Single`, `Married`, `Complicated`, `In a relationship`]
      }
 
 
